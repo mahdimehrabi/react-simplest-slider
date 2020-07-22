caedd43e4f4101b00dc358cf5b298d4bca81671a
mahdi mehrabi <mahdimehrabi17@gmail.com>
2020-07-22 21:29:17 +0430

    base of slider files

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const Slider = (props) => {
+    const [myState, setMyState] = useState([])
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Slider;
