469d13ba3352d902540371b52c51313090c985b3
mahdi mehrabi <mahdimehrabi17@gmail.com>
2020-07-23 11:26:13 +0430

    added base of buttons

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,8 +1,15 @@
-import React, {useState} from "react";
+import React, {useState, Fragment} from "react";
 import classes from "./Slider.css";
 
 const Slider = (props) => {
-    const [myState, setMyState] = useState([])
+    const [myState, setMyState] = useState([]);
+
+    const buttons = (
+        <Fragment>
+            <span className={classes.ButtonRight}></span>
+            <span className={classes.ButtonLeft}></span>
+        </Fragment>
+    );
 
     return (
         <div className={classes.Slider}>
---
+++ src/Components/Slider/UI/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Button.css";
+
+const Slider = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Slider;
